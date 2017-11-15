5efcb260b0b4f0ea90dc792ea9641426c7643ff7
HawkDon <Oliver.Loenning@hotmail.com>
2017-11-15 13:54:04 +0100

    Added database

--- FogProjekt/src/main/java/DataLayer/Connector.java
+++ FogProjekt/src/main/java/DataLayer/Connector.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DataLayer;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
- *
- * @author Christian Kolz Barth
+ * @author PC
  */
 public class Connector {
     
+    private static final String url = "jdbc:mysql://146.185.139.173:3306/jfog";
+    private static final String username = "HawkDon";
+    private static final String password = "Hawk@Don11";
+
+    private static Connection singleton;
+
+    public static Connection connection() throws ClassNotFoundException, SQLException  {
+        if ( singleton == null ) {
+            Class.forName( "com.mysql.jdbc.Driver" );
+            singleton = DriverManager.getConnection( url, username, password );
+        }
+        return singleton;
+    }
 }
---
+++ FogProjekt/src/main/java/DataLayer/MaterialMapper.java
@@ -0,0 +1,74 @@
+package DataLayer;
+
+import FunctionLayer.FittingAndScrews;
+import FunctionLayer.WoodAndRoof;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author PC
+ */
+public class MaterialMapper {
+    public ArrayList<WoodAndRoof> getWoodAndRoof() throws SQLException {
+        
+        ArrayList<WoodAndRoof> WoodandRoof = new ArrayList<WoodAndRoof>();
+        WoodAndRoof WoodsAndRoofs;
+        
+        try {
+            
+            Connection con = Connector.connection();
+            String SQL = "SELECT * FROM woodandroof";
+            PreparedStatement ps = con.prepareStatement(SQL);
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                WoodsAndRoofs = new WoodAndRoof(
+                rs.getString("name"),
+                rs.getInt("length"),
+                rs.getInt("amount"),
+                rs.getString("unit"),
+                rs.getString("description")
+                );
+                WoodandRoof.add(WoodsAndRoofs);
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(MaterialMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return WoodandRoof;
+    }
+    
+    public ArrayList<FittingAndScrews> getFittingAndScrews() throws SQLException {
+        
+        ArrayList<FittingAndScrews> FittingandScrews = new ArrayList<FittingAndScrews>();
+        FittingAndScrews fittingandscrews;
+        
+        try {
+            
+            Connection con = Connector.connection();
+            String SQL = "SELECT * FROM fittingandscrews";
+            PreparedStatement ps = con.prepareStatement(SQL);
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                
+                fittingandscrews = new FittingAndScrews(
+                rs.getString("name"),
+                rs.getInt("amount"),
+                rs.getString("unit"),
+                rs.getString("description")
+                );
+                FittingandScrews.add(fittingandscrews);
+                
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(MaterialMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return FittingandScrews;
+        
+    }
+}
