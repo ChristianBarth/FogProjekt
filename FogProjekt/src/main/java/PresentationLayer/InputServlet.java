a65b8317a965ebefee7136b6fade7a0c58d06ded
Christian Kolz Barth <christian.kolz.barth@gmail.com>
2017-11-16 10:26:59 +0100

    Added inputPage.jsp, confirmationPage.jsp and InputServlet

---
+++ FogProjekt/src/main/java/PresentationLayer/InputServlet.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PresentationLayer;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Christian Kolz Barth
+ */
+@WebServlet(name = "InputServlet", urlPatterns = {"/InputServlet"})
+public class InputServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        String action = request.getParameter("action");
+        
+        if ("build".equals(action)) {
+            String reqLength = request.getParameter("length");
+            int convLength = Integer.parseInt(reqLength);
+            String reqWidth = request.getParameter("width");
+            int convWidth = Integer.parseInt(reqWidth);
+            String reqHeight = request.getParameter("height");
+            int convHeight = Integer.parseInt(reqHeight);
+            
+            sendToConfirmationPage(request, response);
+            return;
+        }
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    private void sendToConfirmationPage (HttpServletRequest request, HttpServletResponse response) {
+        try {
+            RequestDispatcher rd = request.getRequestDispatcher("confirmationPage.jsp");
+            rd.forward(request, response);
+        } catch (IOException | ServletException e) {
+            e.getMessage();
+        }
+    }
+}
---
+++ FogProjekt/src/main/webapp/confirmationPage.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : confirmationPage
+    Created on : 16-11-2017, 09:56:25
+    Author     : Christian Kolz Barth
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>confirmationPage</title>
+    </head>
+    <body>
+        <h1>Vi modtog dine oplysninger</h1>
+    </body>
+</html>
