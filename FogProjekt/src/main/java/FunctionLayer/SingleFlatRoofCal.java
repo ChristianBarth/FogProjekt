f3a8552b9983102935aca8a13e8fc393a94b809b
AsgerSH <asgersh@hotmail.com>
2017-11-16 12:08:18 +0100

    i dun goofd

--- FogProjekt/src/main/java/FunctionLayer/SingleFlatRoofCal.java
+++ FogProjekt/src/main/java/FunctionLayer/SingleFlatRoofCal.java
@@ -5,10 +5,40 @@
  */
 package FunctionLayer;
 
+import DataLayer.MaterialMapper;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  * @author PC
  */
 public class SingleFlatRoofCal {
-    
-    
+
+    public static void main(String[] args) {
+        MaterialMapper mm = new MaterialMapper();
+        mm.getProducts();
+        for (Product product : mm.getProducts()) {
+            System.out.println(product.getLength());
+        }
+                System.out.println("Element: " + mm.getProducts().get(0).getPrice());
+    }
+
+    private MaterialMapper mm = new MaterialMapper();
+
+    public Product Part1(int length, int height, int width) {
+        // mm.getProducts().equals("25x200 mm. trykimp. Brædt");
+        int l;
+        l = mm.getProducts().get(0).getLength();
+        
+        
+
+        return null;
+    }
+
+    public ArrayList<Product> getProducts() {
+        ArrayList<Product> MaterialList = new ArrayList<Product>();
+
+        return MaterialList;
+    }
+
 }
